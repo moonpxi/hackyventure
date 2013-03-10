f75652ffa206c93322f89a31330bf994d2aa6d8c
moonpxi <paulo.schneider@gmail.com>
2013-03-10 19:31:57 +0000

    Formatting header...of some sort

--- scribe.rb
+++ scribe.rb
@@ -1,7 +1,11 @@
 class Scribe
   
   def header(text)
-    puts text    
+    length = text.size + 4
+    puts "#" * length
+    puts "# #{text} #"    
+    puts "#" * length
+    puts
   end
 
 end
