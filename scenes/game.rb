43e0993288c0a71b1ac1f085f3b4543f833d67ee
moonpxi <paulo.schneider@gmail.com>
2013-04-12 16:20:40 +0100

    Setting parameters

--- scenes/game.rb
+++ scenes/game.rb
@@ -2,9 +2,12 @@ require_relative 'scribe'
 require_relative 'story'
 require_relative 'scene'
 
+intro_scene = Scene.new('This is an intro', :end)
+end_scene = Scene.new('The end', nil)
+
 story = Story.new
-story.add_scene(:intro, Scene.new('This is an intro'))
-story.add_scene(:end, Scene.new('The end'))
+story.add_scene(:intro, intro_scene)
+story.add_scene(:end, end_scene)
 
 Scribe.header "Hackyventure/SCENE 0.13.98"
 
